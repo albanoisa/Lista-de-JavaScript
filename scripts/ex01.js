383072967a9bab2e772804ca9dfcd09dd8db3e96
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-03-20 11:41:37 -0300

    add

--- scripts/ex01.js
+++ scripts/ex01.js
@@ -2,17 +2,18 @@ function SomarNumeros() {
     let soma = 0;
     let continuar = true;
     while (continuar) {
-        let numero = parseFloat (prompt ( "Digite um número para somar:" ));
-         if (! isnan (numero)); {
+        let numero = parseFloat(prompt( "Digite um número para somar:" ));
+        if ( !isNaN(numero)); {
         soma += numero;
-
         // soma = soma + numero
-        console.log (soma);
+        console.log(soma);
 
         } else {
           alert( "Por favor, digite um número válido" );
     }
         continuar = confirm ( "Deseja adicionar mais um número?" )
-}
+   }
+
+   alert("A soma dos números é:" + soma);
     
 }
