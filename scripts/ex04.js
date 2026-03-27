86803f8640c5c4a73026ac5ebc242a8cdbcceedc
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-03-27 12:05:11 -0300

    add

---
+++ scripts/ex04.js
@@ -0,0 +1,11 @@
+function Vetor(){
+     let numeros = [];
+     for (let i = 1; i < 5; i++){
+     numeros.push(Number(prompt("Informe o valor:" + i)));   
+
+     }
+     
+     numeros.sort((a, b) => b - a);
+     alert("Os números em ordem decrescente são:" + numeros.join(", "));
+   
+}
