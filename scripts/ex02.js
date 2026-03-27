bb48584b11904e20f674a4694e493b9768103424
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-03-27 11:51:19 -0300

    add

---
+++ scripts/ex02.js
@@ -0,0 +1,9 @@
+function ContagemRegressiva() {
+    alert ("Preparar para a contagem!");
+
+    for (let i = 10, i >=0; i--) {
+        alert("Contagem:" + i)
+    }
+
+    alert("Lançamento Realizado!")
+}
